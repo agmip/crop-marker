3e4883b72292eef577970988689d7115216f8c0d
MengZhang <meng.zhang15@ufl.edu>
2014-10-13 13:48:20 -0400

    Fix the issue when running multiple dataset

--- src/main/java/org/agmip/ui/cropmarker/TestBuilderTask.java
+++ src/main/java/org/agmip/ui/cropmarker/TestBuilderTask.java
@@ -3,7 +3,6 @@ package org.agmip.ui.cropmarker;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import org.agmip.common.Functions;
 import org.agmip.ui.cropmarker.BuildTestPage.DataSpecConfig;
 import static org.agmip.ui.cropmarker.Page.DEF_DATA_PATH;
@@ -31,15 +30,16 @@ public class TestBuilderTask extends Task<TestDefBuilder> {
     private final HashMap<String, DataSpecConfig> dataSpecConfig;
     private final String quaduiPath = ConfigHelper.getQuaduiPath();
     private final String acmouiPath = ConfigHelper.getAcmouiPath();
-    private final String surveyDataPath = DEF_DATA_PATH + "\\%s\\input\\Survey_data.zip";
+    private final String surveyDataPath = DEF_DATA_PATH + "\\%s\\input\\%s.zip";
     private final String fieldDomePath = DEF_DATA_PATH + "\\%s\\input\\Field_Overlay.zip";
     private final String seasonalDomePath = DEF_DATA_PATH + "\\%s\\input\\Seasonal_strategy.zip";
+    private final String linkagePath = DEF_DATA_PATH + "\\%s\\input\\linkage.csv";
     private final String dssat45ExePath = ConfigHelper.getDssat45ExePath();
     private final String modelWorkDir = "*%s.output_dir\\%s";
     private final String dssat45BatchFileName = "dssbatch.v45";
     private final String apsim75ExePath = ConfigHelper.getApsim75ExePath();
-    private final String acmoInputDir = "*%s.output_dir";
-    private final String actualAcmoCsvPath = DEF_DATA_PATH + "\\%s\\expeceted\\%s_%s\\%s";
+    private final String acmoInputDir = "*%s_%s.output_dir";
+    private final String expectedDataPath = DEF_DATA_PATH + "\\%s\\expected\\%s\\";
     private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(TestBuilderTask.class);
 
     public TestBuilderTask(ArrayList<String> testDataList, HashMap<String, DataSpecConfig> dataSpecConfig) {
@@ -77,20 +77,34 @@ public class TestBuilderTask extends Task<TestDefBuilder> {
             }
             String quaduiWorkDir = Functions.revisePath(testConfig.getOutputDir() + File.separator + dataName);
             QuadUIJarRunner quadui = (QuadUIJarRunner) builder.addAppRunner(QUADUI, quaduiPath, quaduiWorkDir, quaduiWorkDir);
-            String quaduiTitle = "QuadUI_" + dataName;
+            String quaduiTitle = dataName + "_QuadUI";
             quadui.setTitle(quaduiTitle);
-            quadui.setArguments("-cli -clean -s " + outputModelStr);
-            quadui.setRawdataPath(String.format(surveyDataPath, dataName));
-            quadui.setLinkPath(" ");
-            quadui.setOverlayPath(String.format(fieldDomePath, dataName));
-            quadui.setSeasonalPath(String.format(seasonalDomePath, dataName));
+            String source = String.format(surveyDataPath, dataName, dataName);
+            String linkage = String.format(linkagePath, dataName);
+            String fDome = String.format(fieldDomePath, dataName);
+            String sDome = String.format(seasonalDomePath, dataName);
+            quadui.setRawdataPath(source);
+            if (new File(fDome).exists()) {
+                quadui.setOverlayPath(fDome);
+                if (new File(linkage).exists()) {
+                    quadui.setLinkPath(linkage);
+                }
+                if (new File(sDome).exists()) {
+                    quadui.setSeasonalPath(sDome);
+                    quadui.setArguments("-cli -clean -s " + outputModelStr);
+                } else {
+                    quadui.setArguments("-cli -clean -f " + outputModelStr);
+                }
+            } else {
+                quadui.setArguments("-cli -clean -n " + outputModelStr);
+            }
             // Models
             for (String model : testConfig.getModels()) {
                 // DSSAT45
                 if (model.equals("DSSAT45")) {
                     String workPath = String.format(modelWorkDir, quaduiTitle, "DSSAT");
                     ExeRunner dssat = (ExeRunner) builder.addAppRunner(EXE, dssat45ExePath, workPath, workPath); // TODO copy dssat input to somewhere
-                    dssat.setTitle(model);
+                    dssat.setTitle(dataName + "_" + model);
                     dssat.setArguments("b", dssat45BatchFileName);
                     // ACMO and comparator
                     setupAcmoAndComp(builder, dataName, model, testConfig);
@@ -98,7 +112,7 @@ public class TestBuilderTask extends Task<TestDefBuilder> {
                 else if (model.equals("APSIM75")) {
                     String workPath = String.format(modelWorkDir, quaduiTitle, "APSIM");
                     ApsimRunner apsim = (ApsimRunner) builder.addAppRunner(APSIM, apsim75ExePath, workPath, workPath); // TODO copy dssat input to somewhere
-                    apsim.setTitle(model);
+                    apsim.setTitle(dataName + "_" + model);
                     apsim.setArguments("AgMip.apsim");
                     // ACMO and comparator
                     setupAcmoAndComp(builder, dataName, model, testConfig);
@@ -115,7 +129,7 @@ public class TestBuilderTask extends Task<TestDefBuilder> {
     private void setupAcmoAndComp(TestDefBuilder builder, String dataName, String model, DataSpecConfig testConfig) {
         // ACMO for Model
         JarRunner acmo = (JarRunner) builder.addAppRunner(JAR, acmouiPath);
-        String acmoTitle = "ACMO_" + model;
+        String acmoTitle = dataName + "_" + model + "_ACMOUI";
         acmo.setTitle(acmoTitle);
         String modelArg = "";
         String modelName = "";
@@ -126,19 +140,34 @@ public class TestBuilderTask extends Task<TestDefBuilder> {
             modelArg = "-apsim";
             modelName = "APSIM";
         }
-        acmo.setArguments("-cli", modelArg, String.format(acmoInputDir, model));
-        if (testConfig.isComparatorSelected("ACMO")) {
-            // Preparing result comparators
-            try {
-                String expectedPath = String.format(actualAcmoCsvPath, dataName, model, "ACMO", "ACMO-MACHAKOS-1-0XFX-0-0-" + modelName + ".csv");
-                TestComparator comparator = builder.addTestComparator(FILE, expectedPath, String.format(acmoInputDir, model) + "\\ACMO-MACHAKOS-1-0XFX-0-0-" + modelName + ".csv");
-                comparator.setTitle("ACMO_" + model + "_Comparator");
-                comparator.setOutputDir(""); // TODO
-            } catch (Exception ex) {
-                LOG.error(Functions.getStackTrace(ex));
+        acmo.setArguments("-cli", modelArg, String.format(acmoInputDir, dataName, model));
+
+        // Comparators
+        for (String comparatorName : testConfig.getComparators()) {
+            if (comparatorName.equalsIgnoreCase("ACMO")) {
+                // Preparing ACMO CSV file comparator
+                try {
+//                    String expectedPath = String.format(expectedDataPath, dataName, model, "ACMO", "ACMO-MACHAKOS-1-0XFX-0-0-" + modelName + ".csv");
+                    String expectedPath = String.format(expectedDataPath, dataName, model);
+                    String actualPath = String.format(acmoInputDir, dataName, model);
+                    File expDir = new File(expectedPath);
+                    File acmoFile = new File(expectedPath + "ACMO-" + modelName + ".csv");
+                    for (File f : expDir.listFiles()) {
+                        String fname = f.getName().toUpperCase();
+                        if (fname.startsWith("ACMO") && fname.endsWith(".CSV")) {
+                            acmoFile = f;
+                            break;
+                        }
+                    }
+                    TestComparator comparator = builder.addTestComparator(FILE, acmoFile.getAbsolutePath(), actualPath + File.separator + acmoFile.getName());
+                    comparator.setTitle(comparatorName + "_" + dataName + "_" + model + "_Comparator");
+                } catch (Exception ex) {
+                    LOG.error(Functions.getStackTrace(ex));
+                }
+            } else if (comparatorName.equalsIgnoreCase("Yield")) {
+                // TODO
+                // Preparing Yield variable comparator
             }
-        } else if (testConfig.isComparatorSelected("Yield")) {
-            // TODO
         }
     }
 }
